4a68ac6288c494b644b4070b20606b45c41bcda2
ahmedsaid67 <ahmetsaiddemirtas@gmail.com>
2024-09-17 13:29:51 +0300

    yüklemek istediğiniz dosya açıklaması

--- pages/Hatirlaticilar.js
+++ pages/Hatirlaticilar.js
@@ -1,22 +1,25 @@
 import React, { useState, useEffect, useRef } from 'react';
-import { View, Text, ScrollView, TouchableOpacity, StyleSheet, Dimensions } from 'react-native';
+import { View, Text, ScrollView, TouchableOpacity, StyleSheet, Dimensions, FlatList, ActivityIndicator } from 'react-native';
 import moment from 'moment';
 import Icon from 'react-native-vector-icons/FontAwesome';
-import 'moment/locale/tr'; // Türkçe dil desteği
-
+import 'moment/locale/tr';
+import { colors } from '../styles/colors';
+import axios from 'axios';
+import { API_ROUTES } from '../utils/constant';
 
 const Hatirlaticilar = () => {
   const [selectedDate, setSelectedDate] = useState(moment().format('YYYY-MM-DD'));
-  const [activeTab, setActiveTab] = useState('Aktif'); // State for active tab
+  const [activeTab, setActiveTab] = useState('Aktif');
+  const [reminders, setReminders] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [page, setPage] = useState(1);
+  const [hasMore, setHasMore] = useState(true);
   const scrollViewRef = useRef();
-
-  // Türkçe dilini ayarlıyoruz
+  
   moment.locale('tr');
 
-  // Cihaz ekran genişliği
   const screenWidth = Dimensions.get('window').width;
 
-  // Tarih aralığını oluştur (30 gün önce ve 30 gün sonrası)
   const getDates = () => {
     let dates = [];
     for (let i = -30; i <= 30; i++) {
@@ -27,17 +30,11 @@ const Hatirlaticilar = () => {
 
   const dates = getDates();
 
-  // Ekran yüklendiğinde bugünün tarihini ortalamak için
   useEffect(() => {
-    // Bugünün tarihini tarihler arasında bul
     const todayIndex = dates.findIndex(date => date === selectedDate);
-
-    // Her bir elemanın genişliği (60px genişlik, + 20px sağ ve sol margin ile birlikte)
-    const itemWidth = 60; // Eleman genişliği
-    const marginHorizontal = 20; // Sağa ve sola margin (10px sağ, 10px sol)
-    const totalItemWidth = itemWidth + marginHorizontal; // Toplam eleman genişliği
-
-    // Elemanı tam ortalamak için hesaplama
+    const itemWidth = 60;
+    const marginHorizontal = 20;
+    const totalItemWidth = itemWidth + marginHorizontal;
     const offset = (todayIndex * totalItemWidth) - (screenWidth / 2) + (totalItemWidth / 2);
 
     if (scrollViewRef.current) {
@@ -45,11 +42,84 @@ const Hatirlaticilar = () => {
     }
   }, [selectedDate]);
 
-  // Bugün tarihi ve seçili tarihleri karşılaştır
   const isToday = selectedDate === moment().format('YYYY-MM-DD');
   const todayLabel = isToday 
     ? `Bugün ${moment(selectedDate).format('D MMMM')}` 
     : moment(selectedDate).format('D MMMM');
+
+  useEffect(() => {
+    const fetchData = async () => {
+      if (loading || !hasMore) return;
+
+      setLoading(true);
+      const endpoint = activeTab === 'Aktif'
+        ? API_ROUTES.USER_REMINDER_ACTİVE
+        : API_ROUTES.USER_REMINDER_INACTIVE;
+        
+      try {
+        const response = await axios.get(endpoint, {
+          params: {
+            date: selectedDate,
+            page: page
+          }
+        });
+        setReminders(prevReminders => [...prevReminders, ...response.data.results]);
+        setHasMore(response.data.next !== null);
+      } catch (error) {
+        console.error("Error fetching reminders:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [page, activeTab, selectedDate]); // `selectedDate` eklenmiş
+
+  const renderReminder = ({ item }) => {
+    const startDate = moment(item.baslangic_tarihi).startOf('day');
+    const endDate = moment(item.bitis_tarihi).endOf('day');
+    const today = moment().startOf('day');
+    
+    let displayText = '';
+    const daysUntilStart = startDate.diff(today, 'days');
+    const daysUntilEnd = endDate.diff(today, 'days');
+
+    if (startDate.isAfter(today)) {
+      displayText = `Başlamasına ${daysUntilStart} gün kaldı`;
+    } else if (startDate.isSameOrBefore(today)) {
+      if (daysUntilEnd >= 0) {
+        displayText = `Tamamlanmasına ${daysUntilEnd} gün kaldı`;
+      } else {
+        displayText = 'Tamamlandı';
+      }
+    }
+
+    return (
+      <View style={styles.reminderContainer}>
+        <View style={styles.reminderContent}>
+          <Icon name="medkit" size={24} color="#1DA1F2" />
+          <View style={styles.reminderTextContainer}>
+            <Text style={styles.reminderText}>{item.name}</Text>
+            <Text style={styles.reminderDetails}>
+              {item.hatirlatici_saat.map((time, index) => (
+                <Text key={index}>{moment(time.saat, 'HH:mm:ss').format('HH:mm')}{index < item.hatirlatici_saat.length - 1 ? ' - ' : ''}</Text>
+              ))}
+            </Text>
+            <Text style={styles.reminderDaysLeft}>
+              {displayText}
+            </Text>
+          </View>
+        </View>
+        <Icon name="clock-o" size={24} color="#1DA1F2" style={styles.reminderIcon} />
+      </View>
+    );
+  };
+
+  const handleEndReached = () => {
+    if (hasMore && !loading) {
+      setPage(prevPage => prevPage + 1);
+    }
+  };
 
   return (
     <View style={styles.container}>
@@ -57,7 +127,7 @@ const Hatirlaticilar = () => {
         <ScrollView
           horizontal
           showsHorizontalScrollIndicator={false}
-          ref={scrollViewRef}  // ScrollView referansı
+          ref={scrollViewRef}
         >
           {dates.map((date, index) => (
             <TouchableOpacity
@@ -66,7 +136,12 @@ const Hatirlaticilar = () => {
                 styles.dateContainer,
                 date === selectedDate ? styles.selectedDate : null,
               ]}
-              onPress={() => setSelectedDate(date)}
+              onPress={() => {
+                setSelectedDate(date);
+                setPage(1); // Reset page number to 1 on date change
+                setReminders([]); // Clear previous reminders
+                setHasMore(true); // Ensure more data can be fetched
+              }}
             >
               <Text style={[ 
                 styles.dateText,
@@ -78,21 +153,23 @@ const Hatirlaticilar = () => {
                 styles.dayText,
                 date === selectedDate ? styles.selectedDateText : null,
               ]}>
-                {moment(date).format('ddd')} {/* Haftanın günü Türkçe gösterilecek */}
+                {moment(date).format('ddd')}
               </Text>
             </TouchableOpacity>
           ))}
         </ScrollView>
-        <Text style={[styles.todayLabel, { color: isToday ? '#1DA1F2' : '#14171A' }]}>
-          {todayLabel}
-        </Text>
+        <Text style={[styles.todayLabel, { color: isToday ? '#1DA1F2' : '#14171A' }]}>{todayLabel}</Text>
       </View>
 
-      {/* Tab Section */}
       <View style={styles.tabContainer}>
         <TouchableOpacity 
           style={[styles.tab, activeTab === 'Aktif' ? styles.activeTab : null]}
-          onPress={() => setActiveTab('Aktif')}
+          onPress={() => {
+            setActiveTab('Aktif');
+            setPage(1); // Reset page number to 1 on tab change
+            setReminders([]); // Clear previous reminders
+            setHasMore(true); // Ensure more data can be fetched
+          }}
         >
           <Text style={[styles.tabText, activeTab === 'Aktif' ? styles.activeTabText : null]}>
             Aktif
@@ -100,7 +177,12 @@ const Hatirlaticilar = () => {
         </TouchableOpacity>
         <TouchableOpacity 
           style={[styles.tab, activeTab === 'Aktif Olmayan' ? styles.activeTab : null]}
-          onPress={() => setActiveTab('Aktif Olmayan')}
+          onPress={() => {
+            setActiveTab('Aktif Olmayan');
+            setPage(1); // Reset page number to 1 on tab change
+            setReminders([]); // Clear previous reminders
+            setHasMore(true); // Ensure more data can be fetched
+          }}
         >
           <Text style={[styles.tabText, activeTab === 'Aktif Olmayan' ? styles.activeTabText : null]}>
             Aktif Olmayan
@@ -108,19 +190,20 @@ const Hatirlaticilar = () => {
         </TouchableOpacity>
       </View>
 
-      <View style={styles.reminderContainer}>
-        <View style={styles.reminderContent}>
-          <Icon name="medkit" size={24} color="#1DA1F2" />
-          <View style={styles.reminderTextContainer}>
-            <Text style={styles.reminderText}>Parol</Text>
-            <Text style={styles.reminderDetails}>Günde 2 kez - 08:00 20:00</Text>
-            <Text style={styles.reminderDaysLeft}>5 gün kaldı</Text>
-          </View>
-        </View>
-        <Icon name="clock-o" size={24} color="#1DA1F2" style={styles.reminderIcon} />
-      </View>
+      <FlatList
+        data={reminders}
+        renderItem={renderReminder}
+        keyExtractor={item => item.id.toString()} // Benzersiz `id` kullanılıyor
+        contentContainerStyle={{ paddingBottom: 100 }}
+        showsVerticalScrollIndicator={false}
+        onEndReached={handleEndReached}
+        onEndReachedThreshold={0.1}
+        ListFooterComponent={loading ? <ActivityIndicator size="small" color={colors.uygulamaRengi} /> : null}
+      />
 
-
+      <TouchableOpacity style={styles.floatingButton}>
+        <Icon name="plus" size={24} color="#fff" />
+      </TouchableOpacity>
     </View>
   );
 };
@@ -186,38 +269,50 @@ const styles = StyleSheet.create({
     color: '#1DA1F2',
   },
   reminderContainer: {
-    position: 'relative', // Allow positioning child elements relative to this container
+    position: 'relative',
     padding: 15,
     backgroundColor: '#fff',
     borderRadius: 10,
     marginTop: 15,
-    marginHorizontal: 15,
+    marginHorizontal: 30,
+    borderWidth: 1,
+    borderColor: colors.border,
   },
   reminderContent: {
-    flexDirection: 'row', // Aligns items horizontally within the content
-    alignItems: 'flex-start', // Align items at the top
+    flexDirection: 'row',
+    alignItems: 'center',
   },
   reminderTextContainer: {
-    marginLeft: 10, // Space between icon and text
+    flex: 1,
+    marginLeft: 15,
   },
   reminderText: {
-    fontSize: 18,
+    fontSize: 16,
     fontWeight: 'bold',
-    color:'#666666',
   },
   reminderDetails: {
     fontSize: 14,
-    color:'#666666',
+    color: '#333',
   },
   reminderDaysLeft: {
     fontSize: 14,
-    color: '#FF0000',
+    color: '#1DA1F2',
   },
   reminderIcon: {
     position: 'absolute',
-    bottom: 10, // Distance from the bottom of the container
-    right: 10, // Distance from the right of the container
+    bottom: 10,
+    right: 10,
+  },
+  floatingButton: {
+    position: 'absolute',
+    bottom: 80,
+    right: 20,
+    backgroundColor: '#1DA1F2',
+    width: 60,
+    height: 60,
+    borderRadius: 30,
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
-
 export default Hatirlaticilar;
--- utils/constant.js
+++ utils/constant.js
@@ -19,35 +19,41 @@ export const API_ROUTES = {
   GOGLE_LOGIN: API_KEY + '/google/',
 
 
-  MEDICINE : API_KEY + '/ilac/?pages=currentPage',  // --> pagination'ludur, tüm ilaçları 20'şer 20'şer döndürür.
-  MEDICINE_BY_CATEGORY : API_KEY + '/ilac/medications_by_category/', // --> pagination'ludur, kategoriye ait ilaçları 20'şer 20'şer döndürür.
-  MEDICINE_BY_CATEGORY_NO_PAGINATION: API_KEY + '/ilac/medications_by_category_no_pagination/',  // --> paginationsuzdur. kategirye ait tüm ilaçları döndürür. kategori sayfalarının searchboxu ıcın kullanaılacak endpoint.
+  MEDICINE : API_KEY + '/ilac/?page=currentPage',  // --> pagination'ludur, tüm ilaçları 20'şer 20'şer döndürür.
+  MEDICINE_BY_CATEGORY : API_KEY + '/ilac/medications-by-category/?page=currentPage', // --> pagination'ludur, kategoriye ait ilaçları 20'şer 20'şer döndürür.
+  MEDICINE_BY_CATEGORY_NO_PAGINATION: API_KEY + '/ilac/medications-by-category-no-pagination/',  // --> paginationsuzdur. kategirye ait tüm ilaçları döndürür. kategori sayfalarının searchboxu ıcın kullanaılacak endpoint.
   
   MEDICINE_CATEGORY : API_KEY + '/ilac-kategori/', // --> paginationsuzdur.
 
-  GET_DOSAGE_BY_AGE : API_KEY + '/yasdoz/get_dosage_by_age',  // yaş doz
-  GET_DOSAGE_BY_WEIGHT : API_KEY + '/kilodoz/get_dosage_by_weight/',  // kilo doz
+  GET_DOSAGE_BY_AGE : API_KEY + '/yasdoz/get-dosage-by-age',  // yaş doz
+  GET_DOSAGE_BY_WEIGHT : API_KEY + '/kilodoz/get-dosage-by-weight/',  // kilo doz
 
-  GET_DOSAGE_BY_EXPLANATION : API_KEY + '/explanationdoz/get_dosage_by_explanation/', // explanation doz
+  GET_DOSAGE_BY_EXPLANATION : API_KEY + '/explanationdoz/get-dosage-by-explanation/', // explanation doz
 
-  GET_DOSAGE_BY_AGE_AND_DISEASE : API_KEY + '/hastalikyasdoz/get_dosage_by_age_and_disease/',  // hastalık yaş doz
-  GET_DOSAGE_BY_WEIGHT_AND_DISEASE : API_KEY + '/hastalikkilodoz/get_dosage_by_weight_and_condition/', // hastalık kilo doz
+  GET_DOSAGE_BY_AGE_AND_DISEASE : API_KEY + '/hastalikyasdoz/get-dosage-by-age-and-disease/',  // hastalık yaş doz
+  GET_DOSAGE_BY_WEIGHT_AND_DISEASE : API_KEY + '/hastalikkilodoz/get-dosage-by-weight-and-condition/', // hastalık kilo doz
 
-  GET_DOSAGE_INCREASING_WEIGHT : API_KEY + '/artankilodoz/get_artan_doz_kilo/',  // artan doz kilo
-  GET_DOSAGE_DECREASING_WEIGHT : API_KEY + '/azalankilodoz/get_azalan_doz_kilo/', // azalan doz kilo
+  GET_DOSAGE_INCREASING_WEIGHT : API_KEY + '/artankilodoz/get-artan-doz-kilo/',  // artan doz kilo
+  GET_DOSAGE_DECREASING_WEIGHT : API_KEY + '/azalankilodoz/get-azalan-doz-kilo/', // azalan doz kilo
 
-  GET_INCREASED_DOSAGE_BY_DISEASE_AND_WEIGHT : API_KEY + '/hastalikartankilodoz/get_hastalik_artan_doz_kilo/', // hastalık ve kıloya gore artan doz
-  GET_DECREASED_DOSAGE_BY_DISEASE_AND_WEIGHT : API_KEY + '/hastalikazalankilodoz/get_hastalik_azalan_doz_kilo/', // hastalık ve kıloya gore azalan doz
+  GET_INCREASED_DOSAGE_BY_DISEASE_AND_WEIGHT : API_KEY + '/hastalikartankilodoz/get-hastalik-artan-doz-kilo/', // hastalık ve kıloya gore artan doz
+  GET_DECREASED_DOSAGE_BY_DISEASE_AND_WEIGHT : API_KEY + '/hastalikazalankilodoz/get-hastalik-azalan-doz-kilo/', // hastalık ve kıloya gore azalan doz
 
-  GET_INCREASED_DOSAGE_BY_DISEASE_AGE_AND_WEIGHT : API_KEY + '/hastalikhemyasahemkiloyabagliartandoz/get_hastalik_artan_doz_hem_kilo_hem_yas/', // hastalık , hem kıloya hem yasa gore artan doz
-  GET_INCREASED_DOSAGE_BY_DISEASE_AGE_AND_WEIGHT_DATA_AGE : API_KEY + '/hastalikhemyasahemkiloyabagliartandoz/get_detail_data/', // eşik olan yaş bilgisini alıyoruz
+  GET_INCREASED_DOSAGE_BY_DISEASE_AGE_AND_WEIGHT : API_KEY + '/hastalikhemyasahemkiloyabagliartandoz/get-hastalik-artan-doz-hem-kilo-hem-yas/', // hastalık , hem kıloya hem yasa gore artan doz
+  GET_INCREASED_DOSAGE_BY_DISEASE_AGE_AND_WEIGHT_DATA_AGE : API_KEY + '/hastalikhemyasahemkiloyabagliartandoz/get-detail-data/', // eşik olan yaş bilgisini alıyoruz
 
-  GET_DECREASING_DOSE_BY_DISEASE_AGE_WEIGHT : API_KEY + '/hastalikhemyasahemkiloyabagliazalandoz/get_hastalik_azalan_doz_hem_kilo_hem_yas/', // hastalık , hem kıloya hem yasa gore azalan doz
-  GET_DECREASING_DOSE_BY_DISEASE_AGE_WEIGHT_DATA_AGE : API_KEY + '/hastalikhemyasahemkiloyabagliazalandoz/get_detail_data/', // eşik olan yaş bilgisini alıyoruz
+  GET_DECREASING_DOSE_BY_DISEASE_AGE_WEIGHT : API_KEY + '/hastalikhemyasahemkiloyabagliazalandoz/get-hastalik-azalan-doz-hem-kilo-hem-yas/', // hastalık , hem kıloya hem yasa gore azalan doz
+  GET_DECREASING_DOSE_BY_DISEASE_AGE_WEIGHT_DATA_AGE : API_KEY + '/hastalikhemyasahemkiloyabagliazalandoz/get-detail-data/', // eşik olan yaş bilgisini alıyoruz
 
-  SUPPLEMENT : API_KEY + '/supplement/', // BESİN TAKVİYELERİ
-  SUPPLEMENT_BY_PRODUCT_CATEGORY : API_KEY + '/productcategory/list_categories_by_supplement/',  // BESİN TAKVİYESİNE GORE ÜRÜN KATEGORİLERİ  -- VİTAMİNLERE DİAR KATEGORİLERİ GETİRİ
-  PRODUCT_CATEGORY_BY_PRODUCT : API_KEY + '/product/list_prodcuts_by_productcategory/', // ÜRÜN KATEGORİSİNE GÖRE ÜRÜNLER -- A VİTAMİNİN DAİR ÜRÜNLERİ GETİRİR
+  SUPPLEMENT : API_KEY + '/supplements/', // BESİN TAKVİYELERİ
+  SUPPLEMENT_BY_PRODUCT_CATEGORY : API_KEY + '/productcategory/list-categories-by-supplement/',  // BESİN TAKVİYESİNE GORE ÜRÜN KATEGORİLERİ  -- VİTAMİNLERE DİAR KATEGORİLERİ GETİRİ
+  PRODUCT_CATEGORY_BY_PRODUCT : API_KEY + '/products/list-products-by-category/', // ÜRÜN KATEGORİSİNE GÖRE ÜRÜNLER -- A VİTAMİNİN DAİR ÜRÜNLERİ GETİRİR
+
+  REMINDERS : API_KEY + '/reminders/',
+  USER_REMINDER_ACTİVE : API_KEY + '/reminders/user-active-reminders/?page=currentPage',
+  USER_REMINDER_INACTIVE : API_KEY + '/reminders/user-inactive-reminders/?page=currentPage',
+  REMINDER_HOURS : API_KEY +'/reminder-hours/',
+  NOTIFICATIONS : API_KEY +'/notifications/',
 
 
 
